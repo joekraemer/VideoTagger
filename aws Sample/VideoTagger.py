366cd29bbdde01f85b3506839c31e847fb386654
joekraemer9 <joek@dmcinfo.com>
2021-01-18 17:21:16 -0800

    Add simple excel manipulation and a list parser.

--- aws Sample/VideoTagger.py
+++ aws Sample/VideoTagger.py
@@ -103,50 +103,134 @@ def ParseLabels(labelList):
     organized = OrganizeTagsByConfidence(noDups)
     return organized
 
+def ListToString(lst):
+    # Takes a lits of strings, in most of these cases, these will be metadata keywords
+    # returns a comma deliniated string of the keywords
+    lstStr = ''
+
+    for item in lst:
+        lstStr += (str(item))
+        lstStr += ', '
+
+    return lstStr
+
 class BlackBoxCSVManager:
     ######
     # This class is used to open and manipulate CSV files used for automatically adding meta data
     ######
     
-    def __init__(self, filePath):
+    def __init__(self, filePath = None):
 
-        if filePath == None:
-            self.CreateCSV(csvTemplate)
+        self.CSVPath = filePath
+        self.Workbook = None
+        self.NameColumn = 1
+        self.KeywordColumn = 3
+
+    def AddVideo(self, vidName, metaDataList = None):
+        # Get the template worksheet 
+        ws = self.Workbook.worksheets[0]
+        
+        # Always insert in the same place
+        ws.insert_rows(3)
+
+        # Write the name 
+        # TODO Make sure that the video file doens't already exist
+        ws['A3'] = vidName
+
+        # TODO Add options to add more data here
+        if metaDataList == None:
+            return
         else:
-            self.CSVPath = filePath
-
-    def AddVideo(self):
-        # Make sure that the video file doens't already exist
-        return
+            self.AddMetadataTags(vidName, metaDataList)
+            return
 
     def RemoveVideoEntry(self):
         # Remove an entry in the csv file
         return
 
-    def CreateCSV(self, template):
+    def CreateCSV(self, template, name):
         # Create a new black box csv file
         # Since we don't have a way to create the file, we copy and rename the Master.xlsx
-        cwdir = os.getcwd()
+        dir = 'C:\VMShared\GitRepos\PersonalProject\VideoTagger\Results'
+        newName = os.path.join(dir,name + '.xlsx')
+        # Copy the template
+        shutil.copy(template, newName)
 
-        shutil.copy(template, cwdir)
+        # head_tail = os.path.split(template)
 
-        head_tail = template
+        # Create the path of the copied file
+        # fileToRename = os.path.join(dir , head_tail[1])
 
-        os.path.join(cwdir , head_tail[1])
+        # Rename the template to our new Job specific name
+        # os.rename(fileToRename, name)
+        
+        self.CSVPath = newName
+        self.Workbook = self.OpenCSV(self.CSVPath)
 
-        newName = 'Job' + (str(time.time_ns()))
-        os.rename(cwdir, newName)
-        return
+        # Return the path of our new xlsx
+        return self.CSVPath
 
-    def LoadCSV(self):
+    def OpenCSV(self, filePath = None ):
+
+        if filePath == None: 
+            filePath = self.CSVPath
         # Open CSV so that the class can edit the entries
-        return
+        return pyxl.load_workbook(filePath)
+
+    def SaveCSV(self, name = None):
+        self.Workbook.save(self.CSVPath)
+
+    def CloseCSV(self):
+        # Close CSV
+        return 
 
     def AddMetadataTags(self, fileName, tags):
         # Add a list tags (str type) to a video entry in the csv
+        # Find the row for the video
+        ws = self.Workbook.worksheets[0]
+        for rw in range(2,ws.max_row+1):
+            cellvalue = ws.cell(row=rw, column = self.NameColumn).value
+            if cellvalue == fileName:
+                # Found a row matching the fileName
+                # Add the list of metadata to the tags column for this row
+                tags_list = ListToString(tags)
+                ws.cell(row=rw, column = self.KeywordColumn).value = tags_list
         return
 
+    def FindRow(self, key, clmn):
+        csv = self.OpenCSV()
+
+        # Given a search key, look through each cell in the column and return the row numbers where it exists
+        for row in range(0, 20):
+                cellValue = csv.cell(column=clmn, row=row, value="{0}".format(get_column_letter(col)))
+                if cellValue == key:
+                    return 
+        
+
 def main():
+
+    # testing manipulating csv
+    tags1 = ['poo', 'pooop', 'POO']
+    tags2 = ['peepee', 'peee', 'PEE']
+    vid1 = 'butts.mp4'
+    vid2 = 'penis.mov'
+
+    name = 'Testing' + str(time.time_ns())
+
+    csvObj = BlackBoxCSVManager()
+    csvObj.CreateCSV(csvTemplate,name)
+
+    csvObj.AddVideo(vid1)
+    csvObj.AddMetadataTags(vid1, tags1)
+    csvObj.AddVideo(vid2,tags2)
+
+    csvObj.SaveCSV()
+
+    # csvObj.FindRow('booty', 1)
+
+
+    if True:
+        return
     print('-'*88)
     print("Welcome to the Amazon Rekognition video detection demo!")
     print('-'*88)
